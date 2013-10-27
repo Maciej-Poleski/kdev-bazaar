b062a38a58708dd0a70da2811e843f9a3e6b27f2
Maciej Poleski <d82ks8djf82msd83hf8sc02lqb5gh5@gmail.com>
2013-10-27 21:11:55 +0100

    Implementation of init, add, commit

--- src/bazaarplugin.cpp
+++ src/bazaarplugin.cpp
@@ -1,23 +1,175 @@
 #include "bazaarplugin.h"
 
+#include <QtCore/QDir>
+
 #include <KPluginFactory>
 #include <KLocale>
 #include <KAboutData>
+#include <vcs/widgets/standardvcslocationwidget.h>
+#include <vcs/dvcs/dvcsjob.h>
+
+#include "importmetadatawidget.h"
 
 using namespace KDevelop;
 
 K_PLUGIN_FACTORY(KDevBazaarFactory, registerPlugin<BazaarPlugin>();)
 K_EXPORT_PLUGIN(KDevBazaarFactory(
-    KAboutData("kdevbazaar", "kdevbazaar", ki18n("Bazaar"),
-               "0.1", ki18n("A plugin to support bazaar version control systems"), KAboutData::License_BSD)
-))
+                    KAboutData("kdevbazaar", "kdevbazaar", ki18n("Bazaar"),
+                               "0.1", ki18n("A plugin to support bazaar version control systems"), KAboutData::License_BSD)
+                ))
+
+static QDir toQDir(const KUrl& url)
+{
+    return QDir(url.toLocalFile());
+}
+
+static QDir workingCopy(const KUrl& path)
+{
+    QDir dir = toQDir(path);
+    while (!dir.exists(".bzr") && dir.cdUp());
+
+    return dir;
+}
 
 BazaarPlugin::BazaarPlugin(QObject* parent, const QVariantList& args) : IPlugin(KDevBazaarFactory::componentData(), parent)
 {
-  Q_UNUSED(args);
+    // TODO: check if there is bzr executable
+
+    KDEV_USE_EXTENSION_INTERFACE( KDevelop::IBasicVersionControl )
+    KDEV_USE_EXTENSION_INTERFACE( KDevelop::IDistributedVersionControl )
+
+    setObjectName("Bazaar");
 }
 
 BazaarPlugin::~BazaarPlugin()
 {
+}
+
+QString BazaarPlugin::name() const
+{
+    return QString::fromUtf8("Bazaar");
+}
+
+VcsJob* BazaarPlugin::add(const KUrl::List& localLocations, IBasicVersionControl::RecursionMode recursion)
+{
+    DVcsJob* job = new DVcsJob(workingCopy(localLocations[0]), this);
+    job->setType(VcsJob::Add);
+    *job << "bzr" << (recursion==Recursive? "add": "add --no-recurse")<< localLocations;
+    return job;
+}
+
+VcsJob* BazaarPlugin::annotate(const KUrl& localLocation, const VcsRevision& rev)
+{
 
 }
+
+VcsJob* BazaarPlugin::commit(const QString& message, const KUrl::List& localLocations, IBasicVersionControl::RecursionMode recursion)
+{
+    if(recursion==Recursive)
+    {
+        QDir dir = workingCopy(localLocations[0]);
+        DVcsJob* job = new DVcsJob(dir, this);
+        job->setType(VcsJob::Commit);
+
+        *job << "bzr" << "commit"<<localLocations << "-m" << message;
+        return job;
+    }
+    else
+    {
+        // TODO: Consider this situation
+    }
+}
+
+VcsJob* BazaarPlugin::copy(const KUrl& localLocationSrc, const KUrl& localLocationDstn)
+{
+
+}
+
+VcsImportMetadataWidget* BazaarPlugin::createImportMetadataWidget(QWidget* parent)
+{
+    return new ImportMetadataWidget(parent);
+}
+
+VcsJob* BazaarPlugin::createWorkingCopy(const VcsLocation& sourceRepository, const KUrl& destinationDirectory, IBasicVersionControl::RecursionMode recursion)
+{
+
+}
+
+VcsJob* BazaarPlugin::diff(const KUrl& fileOrDirectory, const VcsRevision& srcRevision, const VcsRevision& dstRevision, VcsDiff::Type, IBasicVersionControl::RecursionMode recursion)
+{
+
+}
+
+VcsJob* BazaarPlugin::init(const KUrl& localRepositoryRoot)
+{
+    DVcsJob* job = new DVcsJob(toQDir(localRepositoryRoot), this);
+    job->setType(VcsJob::Import);
+    *job << "bzr" << "init";
+    return job;
+}
+
+bool BazaarPlugin::isVersionControlled(const KUrl& localLocation)
+{
+
+}
+
+VcsJob* BazaarPlugin::log(const KUrl& localLocation, const VcsRevision& rev, long unsigned int limit)
+{
+
+}
+
+VcsJob* BazaarPlugin::log(const KUrl& localLocation, const VcsRevision& rev, const VcsRevision& limit)
+{
+
+}
+
+VcsJob* BazaarPlugin::move(const KUrl& localLocationSrc, const KUrl& localLocationDst)
+{
+
+}
+
+VcsJob* BazaarPlugin::pull(const VcsLocation& localOrRepoLocationSrc, const KUrl& localRepositoryLocation)
+{
+
+}
+
+VcsJob* BazaarPlugin::push(const KUrl& localRepositoryLocation, const VcsLocation& localOrRepoLocationDst)
+{
+
+}
+
+VcsJob* BazaarPlugin::remove(const KUrl::List& localLocations)
+{
+
+}
+
+VcsJob* BazaarPlugin::repositoryLocation(const KUrl& localLocation)
+{
+
+}
+
+VcsJob* BazaarPlugin::resolve(const KUrl::List& localLocations, IBasicVersionControl::RecursionMode recursion)
+{
+
+}
+
+VcsJob* BazaarPlugin::revert(const KUrl::List& localLocations, IBasicVersionControl::RecursionMode recursion)
+{
+
+}
+
+VcsJob* BazaarPlugin::status(const KUrl::List& localLocations, IBasicVersionControl::RecursionMode recursion)
+{
+
+}
+
+VcsJob* BazaarPlugin::update(const KUrl::List& localLocations, const VcsRevision& rev, IBasicVersionControl::RecursionMode recursion)
+{
+
+}
+
+VcsLocationWidget* BazaarPlugin::vcsLocation(QWidget* parent) const
+{
+    return new KDevelop::StandardVcsLocationWidget(parent);
+}
+
--- src/bazaarplugin.h
+++ src/bazaarplugin.h
@@ -2,16 +2,42 @@
 #define BAZAAR_PLUGIN_H
 
 #include <interfaces/iplugin.h>
+#include <vcs/interfaces/idistributedversioncontrol.h>
 
 #include <QtCore/QVariantList>
 
-class BazaarPlugin : public KDevelop::IPlugin
+class BazaarPlugin : public KDevelop::IPlugin, public KDevelop::IDistributedVersionControl
 {
     Q_OBJECT
-    
+    Q_INTERFACES(KDevelop::IBasicVersionControl KDevelop::IDistributedVersionControl)
 public:
     explicit BazaarPlugin(QObject *parent, const QVariantList& args = QVariantList());
     virtual ~BazaarPlugin();
+
+    virtual QString name() const;
+
+    virtual KDevelop::VcsJob* add(const KUrl::List& localLocations, RecursionMode recursion);
+    virtual KDevelop::VcsJob* annotate(const KUrl& localLocation, const KDevelop::VcsRevision& rev);
+    virtual KDevelop::VcsJob* commit(const QString& message, const KUrl::List& localLocations, RecursionMode recursion);
+    virtual KDevelop::VcsJob* copy(const KUrl& localLocationSrc, const KUrl& localLocationDstn);
+    virtual KDevelop::VcsImportMetadataWidget* createImportMetadataWidget(QWidget* parent);
+    virtual KDevelop::VcsJob* createWorkingCopy(const KDevelop::VcsLocation& sourceRepository, const KUrl& destinationDirectory, RecursionMode recursion);
+    virtual KDevelop::VcsJob* diff(const KUrl& fileOrDirectory, const KDevelop::VcsRevision& srcRevision, const KDevelop::VcsRevision& dstRevision, KDevelop::VcsDiff::Type, RecursionMode recursion);
+    virtual KDevelop::VcsJob* init(const KUrl& localRepositoryRoot);
+    virtual bool isVersionControlled(const KUrl& localLocation);
+    virtual KDevelop::VcsJob* log(const KUrl& localLocation, const KDevelop::VcsRevision& rev, long unsigned int limit);
+    virtual KDevelop::VcsJob* log(const KUrl& localLocation, const KDevelop::VcsRevision& rev, const KDevelop::VcsRevision& limit);
+    virtual KDevelop::VcsJob* move(const KUrl& localLocationSrc, const KUrl& localLocationDst);
+    virtual KDevelop::VcsJob* pull(const KDevelop::VcsLocation& localOrRepoLocationSrc, const KUrl& localRepositoryLocation);
+    virtual KDevelop::VcsJob* push(const KUrl& localRepositoryLocation, const KDevelop::VcsLocation& localOrRepoLocationDst);
+    virtual KDevelop::VcsJob* remove(const KUrl::List& localLocations);
+    virtual KDevelop::VcsJob* repositoryLocation(const KUrl& localLocation);
+    virtual KDevelop::VcsJob* resolve(const KUrl::List& localLocations, RecursionMode recursion);
+    virtual KDevelop::VcsJob* revert(const KUrl::List& localLocations, RecursionMode recursion);
+    virtual KDevelop::VcsJob* status(const KUrl::List& localLocations, RecursionMode recursion);
+    virtual KDevelop::VcsJob* update(const KUrl::List& localLocations, const KDevelop::VcsRevision& rev, RecursionMode recursion);
+    virtual KDevelop::VcsLocationWidget* vcsLocation(QWidget* parent) const;
+
 };
 
 #endif // BAZAAR_PLUGIN_H
