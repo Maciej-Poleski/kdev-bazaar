bf3398c02c256cdb530163d7e37ecd1fc777d68a
Maciej Poleski <d82ks8djf82msd83hf8sc02lqb5gh5@gmail.com>
2013-12-01 13:43:49 +0100

    Copy support is finished.

--- src/copyjob.cpp
+++ src/copyjob.cpp
@@ -3,12 +3,14 @@
 #include <QtCore/QVariant>
 
 #include <interfaces/iplugin.h>
+#include <vcs/dvcs/dvcsjob.h>
+#include <KIO/CopyJob>
 
-CopyJob::CopyJob(const KUrl& localLocationSrc, const KUrl& localLocationDstn,
-                 KDevelop::IPlugin* parent,
-                 KDevelop::OutputJob::OutputJobVerbosity verbosity)
+#include "bazaarplugin.h"
+
+CopyJob::CopyJob(const KUrl& localLocationSrc, const KUrl& localLocationDstn, BazaarPlugin* parent, KDevelop::OutputJob::OutputJobVerbosity verbosity)
     : VcsJob(parent, verbosity), _plugin(parent), _source(localLocationSrc),
-      _destination(localLocationDstn)
+      _destination(localLocationDstn), _status(KDevelop::VcsJob::JobNotStarted)
 {
     setType(JobType::Copy);
     setCapabilities(Killable);
@@ -22,6 +24,7 @@ KDevelop::IPlugin* CopyJob::vcsPlugin() const
 
 KDevelop::VcsJob::JobStatus CopyJob::status() const
 {
+    return _status;
 }
 
 QVariant CopyJob::fetchResults()
@@ -31,10 +34,46 @@ QVariant CopyJob::fetchResults()
 
 void CopyJob::start()
 {
+    if (_status != KDevelop::VcsJob::JobNotStarted)
+        return;
+    KIO::CopyJob* job = KIO::copy(_source, _destination, KIO::HideProgressInfo);
+    connect(job, SIGNAL(copyingDone(KIO::Job*, KUrl, KUrl, time_t, bool, bool)), this, SLOT(addToVcs(KIO::Job*, KUrl, KUrl, time_t, bool, bool)));
+    _status = KDevelop::VcsJob::JobRunning;
+    _job = job;
+    job->start();
 }
 
 bool CopyJob::doKill()
 {
+    _status = KDevelop::VcsJob::JobCanceled;
+    if (_job)
+        return _job->kill(KJob::Quietly);
+    else
+        return true;
 }
+
+void CopyJob::addToVcs(KIO::Job* job, const KUrl& from, const KUrl& to, time_t mtime, bool directory, bool renamed)
+{
+    (void)job;
+    (void)from;
+    (void)mtime;
+    (void)directory;
+    (void)renamed;
+    if (_status != KDevelop::VcsJob::JobRunning)
+        return;
+    KDevelop::VcsJob* job2 = _plugin->add(to, KDevelop::IBasicVersionControl::Recursive);
+    connect(job2, SIGNAL(result(KJob*)), this, SLOT(finish(KJob*)));
+    _job = job2;
+    job2->start();
+}
+
+void CopyJob::finish(KJob*)
+{
+    _status = KDevelop::VcsJob::JobSucceeded;
+    emitResult();
+    emit resultsReady(this);
+    return;
+}
+
 
 #include "copyjob.moc"
--- src/copyjob.h
+++ src/copyjob.h
@@ -5,12 +5,19 @@
 
 #include <vcs/vcsjob.h>
 
+namespace KIO
+{
+class Job;
+}
+
+class BazaarPlugin;
+
 class CopyJob : public KDevelop::VcsJob
 {
     Q_OBJECT
 
 public:
-    CopyJob(const KUrl& localLocationSrc, const KUrl& localLocationDstn, KDevelop::IPlugin* parent = 0, OutputJobVerbosity verbosity = OutputJob::Verbose);
+    CopyJob(const KUrl& localLocationSrc, const KUrl& localLocationDstn, BazaarPlugin* parent = 0, OutputJobVerbosity verbosity = OutputJob::Verbose);
 
     virtual KDevelop::IPlugin* vcsPlugin() const override;
     virtual KDevelop::VcsJob::JobStatus status() const override;
@@ -20,10 +27,17 @@ public:
 protected:
     virtual bool doKill() override;
 
+private slots:
+    void finish(KJob*);
+    void addToVcs(KIO::Job* job, const KUrl& from, const KUrl& to, time_t mtime, bool directory, bool renamed);
+
 private:
-    KDevelop::IPlugin* _plugin;
+    BazaarPlugin* _plugin;
     KUrl _source;
     KUrl _destination;
+
+    JobStatus _status;
+    QPointer<KJob> _job;
 };
 
 #endif // COPYJOB_H
